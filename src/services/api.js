2495596d4df994e68cf262b093e6f654925ac6e0
Srimurugan <reach.srimurugan@gmail.com>
2026-05-15 13:17:08 +0530

    Fixed Bugs

--- src/services/api.js
+++ src/services/api.js
@@ -4,8 +4,9 @@ const API_KEY = import.meta.env.VITE_YOUTUBE_API_KEY;
 const BASE_URL = 'https://www.googleapis.com/youtube/v3';
 
 export const fetchFromAPI = async (url) => {
+  const separator = url.includes('?') ? '&' : '?';
   try {
-    const { data } = await axios.get(`${BASE_URL}/${url}&key=${API_KEY}`);
+    const { data } = await axios.get(`${BASE_URL}/${url}${separator}key=${API_KEY}`);
     return data;
   } catch (error) {
     console.error('Error fetching from YouTube API', error);
